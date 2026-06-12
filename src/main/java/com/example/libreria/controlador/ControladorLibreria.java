c21fc1256796d55ce67ab497c8291436ba472465
Josedi <158279638+Josedi07@users.noreply.github.com>
2026-06-12 10:33:08 +0200

    Add Libros.java and RepositorioLibros.java

--- src/main/java/com/example/libreria/controlador/ControladorLibreria.java
+++ src/main/java/com/example/libreria/controlador/ControladorLibreria.java
@@ -1,20 +1,43 @@
 package com.example.libreria.controlador;
 
+import com.example.libreria.librosRepositorio.RepositorioLibros;
+import com.example.libreria.modelo.Libros;
 import com.example.libreria.servicio.ServicioLibro;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/libros")
 public class ControladorLibreria {
     private final ServicioLibro libroServicio;
+    private final RepositorioLibros libroRepositorio;
 
-    public ControladorLibreria(ServicioLibro libroServicio) {
+    public ControladorLibreria(ServicioLibro libroServicio, RepositorioLibros libroRepositorio) {
         this.libroServicio = libroServicio;
+        this.libroRepositorio = libroRepositorio;
     }
 
     @GetMapping("/{titulo}")
     public String dameLibro(@PathVariable String titulo){
         return libroServicio.buscarLibro(titulo);
 
+    }
+
+    @GetMapping("/todos")
+    public List<Libros>listarLibros(){
+        return libroRepositorio.findAll();
+
+    }
+
+    // Busca un libro por ID: si existe devuelve 200 OK con el libro, si no existe devuelve 404 Not Found.
+    @GetMapping("/id/{idLibro}")
+    //ResponseEntity sirve para tener el control total de la respuesta HTTP que tu servidor le envía al cliente
+    public ResponseEntity<Libros> listarId(@PathVariable long idLibro){
+        return libroRepositorio.buscaID(idLibro)
+                .map(ResponseEntity::ok)                  // Si lo encuentra -> HTTP 200 OK + JSON
+                .orElse(ResponseEntity.notFound().build());// Si está vacío  -> HTTP 404 NOT FOUND
     }
 }
---
+++ src/main/java/com/example/libreria/librosRepositorio/RepositorioLibros.java
@@ -0,0 +1,36 @@
+package com.example.libreria.librosRepositorio;
+
+import com.example.libreria.modelo.Libros;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class RepositorioLibros {
+
+    private final List<Libros> libros=new ArrayList<>();
+
+    public RepositorioLibros() {
+        libros.add(new Libros(1L,"Miguel de Cervantes", "El Quijote", LocalDate.of(1605, 1, 16)));
+        libros.add(new Libros(2L, "Gabriel García Márquez", "Cien Años de Soledad", LocalDate.of(1967, 5, 30)));
+        libros.add(new Libros(3L, "J.K. Rowling", "Harry Potter y la Piedra Filosofal", LocalDate.of(1997, 6, 26)));
+        libros.add(new Libros(4L, "George Orwell", "1984", LocalDate.of(1949, 6, 8)));
+        libros.add(new Libros(5L, "F. Scott Fitzgerald", "El Gran Gatsby", LocalDate.of(1925, 4, 10)));
+    }
+
+    public List<Libros> findAll(){
+        return libros;
+    }
+
+    //Optional sirve para manejar en la busqueda valores positivos y negativos
+    public Optional<Libros> buscaID(long idLibro){
+        return libros.stream()
+                //Filtra la colección de libros llamandola libros1 y va comparando cada libro con el id pasado por parámetro
+                .filter(libros1 -> libros1.getIdLibro()==idLibro)
+                .findFirst(); //Se queda con el primer resultado
+
+    }
+}
